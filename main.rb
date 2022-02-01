96280e9ff52dde88db9f2b9bec958b2f6ec6d231
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-02-01 13:16:20 +0100

    Add solid principles

--- main.rb
+++ main.rb
@@ -1,34 +1,60 @@
 require './app'
 
-@options = {
-  1 => 'List books',
-  2 => 'List people',
-  3 => 'Create person',
-  4 => 'Create book',
-  5 => 'Create rental',
-  6 => 'List rentals for a given person id',
-  7 => 'Exit'
-}.freeze
-
-def main
-  puts 'Welcome tp School Library App'
-  @app = App.new
-  menu
-end
-
-def menu
-  puts 'Please choose an option by entering a number'
-  @options.each do |key, value|
-    puts "#{key} - #{value}"
-  end
-  user_option = gets.chomp
-
-  if user_option == '7'
-    abort('Thank you for using our Library!')
-  else
-    @app.get_user_option(user_option)
+class Main
+  def initialize
+    @app = App.new
+    @options = {
+      1 => 'List books',
+      2 => 'List people',
+      3 => 'Create person',
+      4 => 'Create book',
+      5 => 'Create rental',
+      6 => 'List rentals for a given person id',
+      7 => 'Exit'
+    }.freeze
+    puts 'Welcome tp School Library App'
     puts
-    menu
+  end
+
+  def display_options
+    puts 'Please choose an option by entering a number'
+    @options.each do |key, value|
+      puts "#{key} - #{value}"
+    end
+  end
+
+  def choose_option(option)
+    case option
+    when 1
+      @app.all_books
+    when 2
+      @app.all_people
+    when 3
+      @app.create_person_input
+    when 4
+      @app.create_book_input
+    when 5
+      @app.create_rental_input
+    when 6
+      @app.rentals_by_id
+    else
+      puts 'Invalid option, Input a valid option'
+      puts
+      main
+    end
+    main
+  end
+
+  def main
+    display_options
+    option = gets.chomp.to_i
+    if option == 7
+      puts '....exiting'
+      sleep 0.1
+      abort('Thank you for using the Library School App!')
+    end
+    choose_option(option)
   end
 end
-main
+
+Main.new.main
